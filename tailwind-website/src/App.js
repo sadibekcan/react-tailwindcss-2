25ee66b2aeb3bbd6bfdd6d8e8f227558ff6313dd
Sadi <43606859+BEKCAN@users.noreply.github.com>
2022-09-27 01:36:57 +0300

    react-tailwindcss
    react-tailwindcss

---
+++ tailwind-website/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Section from "./components/Section";
+
+
+function App() {
+  return (
+    <div className=''>
+      <Header/>
+      <Section/>
+    </div>
+  );
+}
+
+export default App;
